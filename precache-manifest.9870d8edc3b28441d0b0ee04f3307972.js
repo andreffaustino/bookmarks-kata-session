2eb769f7275369bff52dfe4b29f99c18ac7ec66b
André Faustino <andre.faustino@talkdesk.com>
2020-10-22 11:53:26 +0100

    Updates

---
+++ precache-manifest.9870d8edc3b28441d0b0ee04f3307972.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadc5d64e8f5aa2d615b8e0ee184ad76",
+    "url": "/bookmarks-kata-session/index.html"
+  },
+  {
+    "revision": "c00f34c070c8a1e53e16",
+    "url": "/bookmarks-kata-session/static/css/main.1010fad7.chunk.css"
+  },
+  {
+    "revision": "8b5e0306635341124228",
+    "url": "/bookmarks-kata-session/static/js/2.bfb7044a.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/bookmarks-kata-session/static/js/2.bfb7044a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c00f34c070c8a1e53e16",
+    "url": "/bookmarks-kata-session/static/js/main.26876201.chunk.js"
+  },
+  {
+    "revision": "6c6ba6f2eae5879b6f38",
+    "url": "/bookmarks-kata-session/static/js/runtime-main.9aa9434b.js"
+  },
+  {
+    "revision": "cf32229e726e61686f8fad31e2b7a337",
+    "url": "/bookmarks-kata-session/static/media/beer.cf32229e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookmarks-kata-session/static/media/logo.5d5d9eef.svg"
+  }
+]);
